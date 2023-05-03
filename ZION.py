90f07b6482379eec642019bdb25b37ed1a7d4352
AlecK Campos <Aleck.cardoso459@gmail.com>
2023-05-02 23:41:56 -0300

    first commit

---
+++ ZION.py
@@ -0,0 +1,10 @@
+##times = [sao paulo, Corinthians, Palmeiras]
+campeonato_paulista = ['palmeiras','corinthians', 'sao paulo']
+campeonato_brasilerao = ['sao paulo','palmeiras', 'corinthians']
+campeonato_paulista.append('santos')
+campeonato_paulista.remove ('sao paulo')
+print (campeonato_paulista)
+print ('campeao do campeonato paulista', campeonato_paulista [0], '\nvice campeao', campeonato_paulista[1])
+print ('campeao do campeonato brasilerao', campeonato_brasilerao [0], '\nvice campeao', campeonato_brasilerao[1])
+
+print ('campeonato paulista', campeonato_paulista)
---
+++ programa.py
@@ -0,0 +1,36 @@
+##TESTE
+a = 10
+b = 5
+soma = a + b
+
+##TESTE
+print("Soma: ", soma)
+
+
+a= 50
+b=15
+c=60
+
+subitracao = a - b * c
+
+print ('multiplicacao e subritracao a soma é ',soma )
+
+
+a =12
+b=40
+c=56
+d=80 
+seila = [80, 10, 30]
+print (seila[2])
+
+
+
+## teste3
+ 
+
+ a=80
+ b=20
+ c=60
+ 
+ soma = a + b
+print ('soma total ',soma)
